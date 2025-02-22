fce94596ab16ae8a45ecbbeb7e17f0606e399eef
Gláucio Oliveira <glaucio.dev@gmail.com>
2025-02-22 02:05:44 -0300

    adding savemovie validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -21,6 +21,25 @@ const savePlaylist = async (req, res, next) => {
     }).catch( err => console.log(err))
 }
 
+const saveMovie = async (req, res, next) => {
+    const validationRule = {
+        name: "required|string"
+    };
+
+    await validator(req.body, validationRule, {}, (err, status) => {
+        if (!status) {
+            res.status(412)
+                .send({
+                    success: false,
+                    message: 'Validation failed',
+                    data: err
+                });
+        } else {
+            next();
+        }
+    }).catch( err => console.log(err))
+}
+
 module.exports = {
-    savePlaylist
+    savePlaylist, saveMovie
 };
