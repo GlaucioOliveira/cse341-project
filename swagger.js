476eb127745f46b0681df8ad3ecd7e1b980afa9a
Gláucio Oliveira <glaucio.dev@gmail.com>
2025-03-03 13:48:21 -0300

    changing swagger file to use oauth

--- swagger.js
+++ swagger.js
@@ -6,9 +6,24 @@ const doc = {
     description: 'WatchList Movies API Information',
   },
   host: 'cse341-project-yrv7.onrender.com',
-  "schemes": [
-    "https"
-  ],
+  schemes: ['https'],
+  securityDefinitions: {
+    OAuth2: {
+      type: 'oauth2',
+      description: 'OAuth2 authentication using Auth0',
+      flow: 'accessCode',
+      authorizationUrl: 'https://dev-2ksmol8sfp1zk1dy.us.auth0.com/authorize',
+      tokenUrl: 'https://dev-2ksmol8sfp1zk1dy.us.auth0.com/oauth/token',
+      scopes: {
+        'write': 'Write access'
+      }
+    }
+  },
+  security: [
+    {
+      OAuth2: ['write']
+    }
+  ]  
 };
 
 const outputFile = './swagger-output.json';
